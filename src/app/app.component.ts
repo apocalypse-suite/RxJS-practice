c29275ca7d95e5a229b43d1194950eeef279a293
apocalypse-suite <mitchenko.catherine@gmail.com>
2021-05-08 22:11:39 +0300

    OBSERVABLES, OBSERVERS & SUBSCRIPTIONS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild, ElementRef, AfterViewInit, OnInit} from '@angular/core';
+import {fromEvent, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'RxJS-practice';
+  @ViewChild('btn') button: ElementRef;
+
+  ngOnInit(): void {
+
+    const myObserver = {
+      next: (x: string) => console.log('Observer got a next value: ' + x),
+      error: (err: string) => console.error('Observer got an error: ' + err),
+      complete: () => console.log('Observer got a complete notification')
+    };
+
+    new Observable((observer) => {
+      observer.next('A value');
+      setTimeout(() => {
+        observer.complete();
+      }, 2000);
+      observer.next('A second value');
+    }).subscribe(myObserver);
+
+  }
+
+
+  ngAfterViewInit(): void {
+    const subscription = fromEvent(this.button.nativeElement, 'click')
+      .subscribe((event: MouseEvent) => console.log(event.clientX));
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 5000);
+  }
+
 }
